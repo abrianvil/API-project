338347857be4c0df3c9f64c3341f1c4a7882bc80
abel <abrianvil@gmail.com>
2022-09-23 13:29:13 -0400

    Added firstName LastName attributes to model and migration

--- backend/db/seeders/20220922180857-demo-user.js
+++ backend/db/seeders/20220922180857-demo-user.js
@@ -8,16 +8,22 @@ module.exports = {
       {
         email: 'demo@user.io',
         username: 'Demo-lition',
+        firstName: 'Demo_firstName',
+        lastName: 'Demo_lastName',
         hashedPassword: bcrypt.hashSync('password')
       },
       {
         email: 'user1@user.io',
         username: 'FakeUser1',
+        firstName: 'FakeUser_firstName',
+        lastName: 'FakeUser_lastName',
         hashedPassword: bcrypt.hashSync('password2')
       },
       {
         email: 'user2@user.io',
         username: 'FakeUser2',
+        firstName: 'FakeUser2_firstName',
+        lastName: 'FakeUser2_lastName',
         hashedPassword: bcrypt.hashSync('password3')
       }
     ], {});
--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -26,13 +26,19 @@ const validateSignup = [
         .exists({ checkFalsy: true })
         .isLength({ min: 6 })
         .withMessage('Password must be 6 characters or more.'),
+    check('firstName')
+        .exists({ checkFalsy: true })
+        .withMessage('Please provide a firstName.'),
+    check('lastName')
+        .exists({ checkFalsy: true })
+        .withMessage('Please provide a lastName.'),
     handleValidationErrors
 ];
 
 // Sign up
 router.post('/', validateSignup, async (req, res) => {
-    const { email, password, username } = req.body;
-    const user = await User.signup({ email, username, password });
+    const { firstName, lastName, email, password, username } = req.body;
+    const user = await User.signup({ firstName, lastName, email, username, password });
 
     await setTokenCookie(res, user);
 
