c6febec0a23b923da7e2ba50afbecb47ceeae717
abel <abrianvil@gmail.com>
2022-09-27 12:32:38 -0400

    model refactor

--- backend/db/seeders/20220927080111-demo-booking.js
+++ backend/db/seeders/20220927080111-demo-booking.js
@@ -15,14 +15,14 @@ module.exports = {
     {
       spotId:1,
       userId:5,
-      startDate:'2023-02-15',
-      endDate:'2023-02-31'
+      startDate:new Date('2023-02-15'),
+      endDate:new Date('2023-02-31')
     },
     {
       spotId:4,
       userId:4,
-      startDate:'2022-12-10',
-      endDate:'2022-12-25'
+      startDate:new Date('2022-12-10'),
+      endDate:new Date('2022-12-25')
     }
    ])
   },
