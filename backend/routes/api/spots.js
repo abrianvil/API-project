d28a05ffa8346c8be827f50e93894755b6340fc1
abel <abrianvil@gmail.com>
2022-09-28 00:27:27 -0400

    edit a spot- ready for review

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -8,6 +8,37 @@ const router = express.Router();
 
 router.use(express.json());
 
+const validateSpotData = [
+    check('address')
+        .exists({ checkFalsy: true })
+        .withMessage("Street address is required"),
+    check('city')
+        .exists({ checkFalsy: true })
+        .withMessage("City is required"),
+    check('state')
+        .exists({ checkFalsy: true })
+        .withMessage("State is required"),
+    check('country')
+        .exists({ checkFalsy: true })
+        .withMessage("Country is required"),
+    // check('lat')
+    //     .exists({ checkFalsy: true })
+    //     .withMessage("Latitude is not valid"),
+    // check('lng')
+    //     .exists({ checkFalsy: true })
+    //     .withMessage("Longitude is not valid"),
+    check('name')
+        .exists({ checkFalsy: true })
+        .withMessage("Name must be less than 50 characters"),
+    check('description')
+        .exists({ checkFalsy: true })
+        .withMessage("Description is required"),
+    check('price')
+        .exists({ checkFalsy: true })
+        .withMessage("Price per day is required"),
+    handleValidationErrors
+];
+
 
 // Get all Spots owned by the Current User
 router.get('/current', requireAuth, async (req, res, next) => {
@@ -84,6 +115,35 @@ router.post('/:spotId/images', requireAuth, async (req, res, next) => {
     }
 })
 
+// Edit a Spot
+router.put('/:spotId', validateSpotData, async (req, res, next) => {
+    const spot = await Spot.findByPk(req.params.spotId)
+    if (spot) {
+        const { address, city, state, country, lat, lng, name, description, price } = req.body
+        const updated = spot.update({
+            address,
+            city,
+            state,
+            country,
+            lat,
+            lng,
+            name,
+            description,
+            price
+        })
+
+        res.status(200)
+        res.json(spot)
+    } else {
+        const error = {
+            message: "Spot couldn't be found",
+            statusCode: 404
+        }
+        next(error)
+    }
+
+})
+
 
 // Get details of a Spot from an id
 router.get('/:spotId', async (req, res, next) => {
@@ -134,36 +194,7 @@ router.get('/:spotId', async (req, res, next) => {
 
 // Create a Spot=====>Error handler missing
 
-const validateSpotData = [
-    check('address')
-        .exists({ checkFalsy: true })
-        .withMessage("Street address is required"),
-    check('city')
-        .exists({ checkFalsy: true })
-        .withMessage("City is required"),
-    check('state')
-        .exists({ checkFalsy: true })
-        .withMessage("State is required"),
-    check('country')
-        .exists({ checkFalsy: true })
-        .withMessage("Country is required"),
-    // check('lat')
-    //     .exists({ checkFalsy: true })
-    //     .withMessage("Latitude is not valid"),
-    // check('lng')
-    //     .exists({ checkFalsy: true })
-    //     .withMessage("Longitude is not valid"),
-    check('name')
-        .exists({ checkFalsy: true })
-        .withMessage("Name must be less than 50 characters"),
-    check('description')
-        .exists({ checkFalsy: true })
-        .withMessage("Description is required"),
-    check('price')
-        .exists({ checkFalsy: true })
-        .withMessage("Price per day is required"),
-    handleValidationErrors
-];
+
 
 
 router.post('/', validateSpotData, async (req, res, next) => {
