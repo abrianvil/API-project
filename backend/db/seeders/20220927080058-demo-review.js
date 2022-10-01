af4bd3645ac9c08dd96570277ed099649f1aad4c
abel <abrianvil@gmail.com>
2022-09-30 20:43:41 -0400

    ready for live testing

--- backend/db/seeders/20220927080058-demo-review.js
+++ backend/db/seeders/20220927080058-demo-review.js
@@ -14,16 +14,35 @@ module.exports = {
     await queryInterface.bulkInsert('Reviews', [
       {
         spotId: 4,
-        userId: 4,
+        userId: 5,
         review: 'the best place on the entire planet',
         stars: 5
       },
       {
         spotId: 5,
-        userId: 1,
+        userId: 4,
         review: 'i was going to ask for a refund, but i am just happy i came out alive',
-        stars: 0
+        stars: 2
+      },
+      {
+        spotId: 3,
+        userId: 3,
+        review: 'just another review',
+        stars: 4
+      },
+      {
+        spotId: 1,
+        userId: 2,
+        review: 'i really hope i pass',
+        stars: 3
+      },
+      {
+        spotId: 2,
+        userId: 1,
+        review: 'i mean really, this hurricane sucked!',
+        stars: 5
       }
+
     ], {})
 
   },
