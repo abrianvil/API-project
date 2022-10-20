477f36cbfe7f9d624bbd2439690b943b69458631
abel <abrianvil@gmail.com>
2022-10-20 01:33:39 -0400

    very

--- frontend/src/components/SpotDetails/index.js
+++ frontend/src/components/SpotDetails/index.js
@@ -2,7 +2,7 @@ import { useSelector, useDispatch } from 'react-redux'
 import { useEffect, useState } from 'react'
 import { useParams, useHistory } from 'react-router-dom'
 import { getASpot } from '../../store/spots'
-
+import { Modal } from '../../context/Modal'
 import { deleteASpot } from '../../store/spots'
 import { clearState } from '../../store/spots'
 import ReviewFormModal from '../reviewFormModal'
@@ -66,42 +66,42 @@ function ShowDetails() {
                     </div>
                     <div className='description-card'>
                         <div className='info-box'>
-                            <div>
-                                <div className='describe-div'>
-                                    <div >
-                                        <h3>Home Hosted By {spotDetail.firstName}</h3>
-                                        <p>{spotDetail.description}</p>
-                                    </div>
-                                    <div>
-                                        <h3>Self check-in</h3>
-                                        <p>Check yourself in with the lockbox.</p>
-                                    </div>
-                                    <h2>Reviews</h2>
-                                    <ul>
-                                        <li>Abel put the reviews here</li>
-                                    </ul>
 
+                            <div className='describe-div'>
+                                <div >
+                                    <h3>Home Hosted By {spotDetail.firstName}</h3>
+                                    <p>{spotDetail.description}</p>
                                 </div>
-                                <fieldset>
-                                    <div className='priceReview'>
-                                        <h2>${spotDetail.price} night</h2>
-                                        <p>⭐{spotDetail.avgStarRating} .{spotDetail.numReviews} reviews</p>
-                                    </div>
-                                    <h3>Free cancellation</h3>
-                                    <button className='buttonGroup' onClick={() => setShowForm(true)}>Add a Review</button>
-                                    {showForm && (<Modal onClose={() => setShowForm(false)} id='review-form'>
-                                        <ReviewFormModal />
-                                    </Modal>)}
-                                    <button hidden={(user.id === spotDetail.ownerId ? false : true)}
-                                        onClick={onEdit}
-                                        className='buttonGroup'>Edit Spot
-                                    </button>
+                                <div>
+                                    <h3>Self check-in</h3>
+                                    <p>Check yourself in with the lockbox.</p>
+                                </div>
+                                <h2>Reviews</h2>
+                                <ul>
+                                    <li>Abel put the reviews here</li>
+                                </ul>
 
-                                    <button hidden={(user.id === spotDetail.ownerId ? false : true)}
-                                        onClick={onDelete} className='buttonGroup'>Delete Spot
-                                    </button>
-                                </fieldset>
                             </div>
+                            <fieldset>
+                                <div className='priceReview'>
+                                    <h2>${spotDetail.price} night</h2>
+                                    <p>⭐{spotDetail.avgStarRating} .{spotDetail.numReviews} reviews</p>
+                                </div>
+                                <h3>Free cancellation</h3>
+                                <button className='buttonGroup' onClick={() => setShowForm(true)}>Add a Review</button>
+                                {showForm && (<Modal onClose={() => setShowForm(false)} id='review-form'>
+                                    <ReviewFormModal />
+                                </Modal>)}
+                                <button hidden={(user.id === spotDetail.ownerId ? false : true)}
+                                    onClick={onEdit}
+                                    className='buttonGroup'>Edit Spot
+                                </button>
+
+                                <button hidden={(user.id === spotDetail.ownerId ? false : true)}
+                                    onClick={onDelete} className='buttonGroup'>Delete Spot
+                                </button>
+                            </fieldset>
+
                         </div>
                     </div>
                 </div>
